754b1432e35f952439357e06ad995885c8ab1eea
Hai Nam <hainam2016@gmail.com>
2023-10-01 14:18:46 +0700

    Initialize Code

---
+++ simulation.m
@@ -0,0 +1,32 @@
+% Measurements generating for GM-PHD Multiple Extended Object Tracking
+% --------------------------------------------------------------------
+% Object numbers: 2
+% Transition noise, measurement noise: Gauss
+% False Alarm: Poisson
+% Birth:
+
+%% Simulation setting
+
+
+
+%% Ground-truth, noise setting
+
+
+
+%% Prior
+
+
+
+%% Measurement Update
+
+
+
+%% Plot and visualize
+
+
+
+%% Evaluation
+
+
+
+%% 
