9685007d7a9791fc82a844ddc55c59dcf89fcf7e
Hai Nam <hainam2016@gmail.com>
2023-10-11 10:10:11 +0700

    Extend birth probability

--- gen_model.m
+++ gen_model.m
@@ -36,10 +36,19 @@ model.w_birth= zeros(model.L_birth,1);                                %weights o
 model.m_birth= zeros(model.xdim,model.L_birth);                       %means of Gaussian birth terms 
 model.P_birth= zeros(model.xdim,model.xdim,model.L_birth);            %cov of Gaussian birth terms
 
-model.w_birth= [.01; .01];                                              %birth term 1
-model.m_birth= [ [647; -435; 0; 0] [-250; -250; 0; 0] ];
+model.w_birth= [.01; .01];                                            %birth of simulation (example 1)
+model.m_birth= [ [250; 250; 0; 0] [-250; -250; 0; 0] ];
 model.P_birth(:,:,1)= diag([100, 100, 25, 25]);
 model.P_birth(:,:,2)= diag([100, 100, 25, 25]);
+
+model.w_birth_2= [.01; .01];                                          %birth of simulation_2
+model.m_birth_2= [ [647; -435; 0; 0] [-250; -250; 0; 0] ];
+model.P_birth_2(:,:,1)= diag([100, 100, 25, 25]);
+model.P_birth_2(:,:,2)= diag([100, 100, 25, 25]);
+
+model.w_birth_3= .01;                                                 %birth of comparison
+model.m_birth_3= [0; 0; 0; 0] ;
+model.P_birth_3= diag([20, 20, 10, 10]);
 
 % Detection parameters
 model.P_D = 0.99;       % probability of detection in measurements
@@ -47,5 +56,5 @@ model.P_MD= 1-model.P_D; % probability of missed detection in measurements
 
 % Clutter parameters
 model.lambda_c = 50; % clutter rate
-model.range_c= [-400 1000; -1000 400];      % uniform clutter region
+model.range_c= [-1000 1000; -1000 1000];      % uniform clutter region
 model.pdf_c= 1/prod(model.range_c(:,2)-model.range_c(:,1)); % uniform clutter density
